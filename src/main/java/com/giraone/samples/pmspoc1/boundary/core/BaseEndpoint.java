1020be2667256f2c00b73dc680e994fcf321e2ad
giraone <hans.schuell@giraone.com>
2015-11-17 23:23:40 +0100

    Support for CORS pre-flight requests added

--- src/main/java/com/giraone/samples/pmspoc1/boundary/core/BaseEndpoint.java
+++ src/main/java/com/giraone/samples/pmspoc1/boundary/core/BaseEndpoint.java
@@ -4,6 +4,7 @@ import javax.ws.rs.OPTIONS;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.ResponseBuilder;
 
 /**
  * TODO: Only for non-production!
@@ -12,24 +13,32 @@ public class BaseEndpoint
 {
 	protected final static String DEFAULT_PAGING_SIZE = "20";
 	
-	// Match root-resources
+	// Matches root-resources also - handle CORS pre-flight request
 	@OPTIONS
 	public Response options()
 	{
 	    return Response.status(Response.Status.NO_CONTENT)
 	    	.header("Access-Control-Allow-Origin", "*") // "127.0.0.1" does not work
-			.header("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT")
+	    	//.header("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT, OPTIONS")
+			.header("Access-Control-Allow-Methods", "*")
 	    	.build();
 	}
 	
-	// Match sub-resources
+	// Match sub-resources - handle CORS pre-flight request
 	@OPTIONS
 	@Path("{path:.*}")
 	public Response optionsAll(@PathParam("path") String path)
 	{
 	    return Response.status(Response.Status.NO_CONTENT)
 	    	.header("Access-Control-Allow-Origin", "*") // "127.0.0.1" does not work
-			.header("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT")
+			.header("Access-Control-Allow-Methods", "*")
 			.build();
+	}
+	
+	// No more used - see CorsFeature class
+	public Response buildWithCorsHeader(ResponseBuilder builder)
+	{	        
+		builder.header("Access-Control-Allow-Origin", "*");     
+        return builder.build();
 	}
 }
