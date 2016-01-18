9406a4227c8e474908fea90183dcabded1cf0ede
giraone <hans.schuell@giraone.com>
2016-01-18 16:47:50 +0100

    Validator class marked as TODO.

--- src/main/java/com/giraone/samples/common/entity/enums/StringEnumeration.java
+++ src/main/java/com/giraone/samples/common/entity/enums/StringEnumeration.java
@@ -1,10 +1,13 @@
 package com.giraone.samples.common.entity.enums;
 
 import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 
 import javax.validation.Constraint;
 import javax.validation.Payload;
 
+// TODO: This is only a marker, that this kind of string enumeration validation is needed.
 /**
  * An annotation to mark attributes of an entity as being stored as a string based data type in the
  * relational database. Usually using <i>varchar</i> or <char> SQL data type.
@@ -12,8 +15,9 @@ import javax.validation.Payload;
  */
 @Documented
 @Constraint(validatedBy = StringEnumerationValidator.class)
+@Retention(RetentionPolicy.RUNTIME)
 public @interface StringEnumeration
-{
+{	
 	String message() default "{com.xxx.bean.validation.constraints.StringEnumeration.message}";
 
 	Class<?>[] groups() default {};
